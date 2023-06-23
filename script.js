99268b6f4a2dd5dbf186ea0634da0b51bd70032a
OSCARJMG23 <oscarjulianm2112@gmail.com>
2023-06-23 14:43:51 -0500

    por terminar

--- script.js
+++ script.js
@@ -1,81 +1,73 @@
-class Task {
-  constructor(name, description, dueDate) {
-    this.name = name;
-    this.description = description;
-    this.dueDate = dueDate;
-    this.completed = false;
+class Quiz{
+  constructor(formulario){
+    this.pregunta = formulario.pregunta.value;
+    this.a = formulario.a.value;
+    this.b = formulario.b.value;
+    this.c = formulario.c.value;
+    this.d = formulario.c.value;
+    this.correcta = formulario.correcta.value;
+    this.respondida = false;
   }
-
-  toggleCompleted() {
-    this.completed = !this.completed;
-  }
-}
-
-class TaskManager {
-  constructor() {
-    this.tasks = [];
-  }
-
-  addTask(task) {
-    this.tasks.push(task);
-  }
-
-  removeTask(index) {
-    this.tasks.splice(index, 1);
-  }
-
-  getTasks() {
-    return this.tasks;
+  responder(){
+    this.respondida = true;
   }
 }
-
-// Crear instancia del administrador de tareas
-const taskManager = new TaskManager();
-
-// Lógica de interacción con el DOM
-const taskForm = document.getElementById("task-form");
-const taskNameInput = document.getElementById("task-name");
-const taskDescInput = document.getElementById("task-description");
-const taskDueDateInput = document.getElementById("task-due-date");
-const taskList = document.getElementById("task-list");
-
-taskForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-  
-  const name = taskNameInput.value;
-  const description = taskDescInput.value;
-  const dueDate = taskDueDateInput.value;
-  const task = new Task(name, description, dueDate);
-  taskManager.addTask(task);
-  renderTasks();
-  taskForm.reset();
+class QuizManager{
+  constructor(){
+    this.quizes = [];
+  }
+   addQuiz(param){
+    this.quizes.push(param)
+  }
+}
+const form = document.getElementById("task-form");
+const quizManager = new QuizManager();
+const quizLista = document.getElementById("quiz-list");
+form["subir"].addEventListener("click", (ev) => {
+  ev.preventDefault()
+  const quiz = new Quiz(form);
+  quizManager.addQuiz(quiz);
+  console.log(quizManager.quizes);
+  quizRender(form);
+  form.reset();
 });
-
-function renderTasks() {
-  console.log(taskManager.tasks)
-  taskList.innerHTML = "";
-  const tasks = taskManager.getTasks();
-  for (let i = 0; i < tasks.length; i++) {
-    const task = tasks[i];
-    const listItem = document.createElement("li");
-    listItem.innerHTML = `
-      <input type="checkbox" ${task.completed ? "checked" : ""} onchange="toggleTaskCompleted(${i})">
-      <span>${task.name}</span>
-      <button onclick="removeTask(${i})">Eliminar</button>
-    `;
-    taskList.appendChild(listItem);
+function quizRender(param){
+  quizLista.innerHTML += `
+    <div>
+      Pregunta : ${param.pregunta.value}
+      <form id = "formulario${quizManager.quizes.length}">
+        <div style = "display:block"> 
+          <label>A: ${param.a.value} </label>
+          <input type = "radio" name = "respuesta" value="a" onchange = "verificar(this)">
+        </div>
+        <div style = "display:block">
+          <label>B: ${param.b.value} </label>
+          <input type = "radio" name = "respuesta" value="b" onchange = "verificar(this)" >
+        </div>
+        <div style = "display:block">
+          <label>C: ${param.c.value} </label>
+          <input type = "radio" name = "respuesta" value="c" onchange = "verificar(this)" >
+        </div>
+        <div style = "display:block">
+          <label>D: ${param.d.value} </label>
+          <input type = "radio" name = "respuesta" value="d" onchange = "verificar(this)">
+        </div>
+      </form>
+    </div>
+  `;
+}
+function verificar(param){
+  let id = parseInt(param.parentNode.getAttribute("id").match(/[0-9]/gi)) - 1;
+  quizManager.quizes[id].responder(); 
+  console.log(param.value);
+  if(quizManager.quizes[id].correcta == param.value){
+    alert("respuesta correcta");
   }
+  else{
+    alert("respuesta incorrecta");
+  } 
+  [...param.parentNode].map(e => {
+      e.disabled = "true";
+    })
+  console.log(quizManager);
 }
-
-function toggleTaskCompleted(index) {
-  const task = taskManager.getTasks()[index];
-  task.toggleCompleted();
-  renderTasks();
-}
-
-function removeTask(index) {
-  taskManager.removeTask(index);
-  renderTasks();
-}
-
-renderTasks();
