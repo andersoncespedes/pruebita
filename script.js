e8fb95f0aef935657bcfc6a96e12d5c3fd1a5aae
jose luis <joseluiscor2015@gmail.com>
2023-06-23 19:57:24 -0500

    a

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 class Quiz{
-  constructor(formulario){
+  constructor(formulario,id){
+    this.id = id;
     this.pregunta = formulario.pregunta.value;
     this.a = formulario.a.value;
     this.b = formulario.b.value;
@@ -19,23 +20,32 @@ class QuizManager{
    addQuiz(param){
     this.quizes.push(param)
   }
+  deleteQuiz(param,id){
+     console.log(param.parentNode.parentNode.parentNode);
+    this.quizes = [...this.quizes].filter((e,i) => e.id != id );
+    param.parentNode.parentNode.parentNode.className = "";
+    param.parentNode.parentNode.parentNode.innerHTML = "";
+    console.log(quizManager.quizes);
+  }
 }
 const form = document.getElementById("task-form");
 const quizManager = new QuizManager();
 const quizLista = document.getElementById("quiz-list");
-form["subir"].addEventListener("click", (ev) => {
+let id = 0;
+form.addEventListener("submit", (ev) => {
   ev.preventDefault()
-  const quiz = new Quiz(form);
+  const quiz = new Quiz(form,id);
   quizManager.addQuiz(quiz);
   console.log(quizManager.quizes);
   quizRender(form);
+  id++;
   form.reset();
 });
 function quizRender(param){
   quizLista.innerHTML += `
-    <div>
+    <div class = "col-md-4">
       Pregunta : ${param.pregunta.value}
-      <form id = "formulario${quizManager.quizes.length}">
+      <form id = "formulario${quizManager.quizes[quizManager.quizes.length - 1].id}">
         <div style = "display:block"> 
           <label>A: ${param.a.value} </label>
           <input type = "radio" name = "respuesta" value="a" onchange = "verificar(this)">
@@ -52,23 +62,39 @@ function quizRender(param){
           <label>D: ${param.d.value} </label>
           <input type = "radio" name = "respuesta" value="d" onchange = "verificar(this)">
         </div>
+        <div>
+          <button type = "button" class = "btn btn-warning" >Editar</button>
+          <button type = "button" class = "btn btn-danger" onclick = "eliminar(this)">Eliminar</button>
+        </div>
       </form>
     </div>
   `;
 }
-function verificar(param){
-  let id = parseInt(param.parentNode.parentNode.getAttribute("id").match(/[0-9]/gi)) - 1;
+function generarid(param){
+  return parseInt(param.parentNode.parentNode.getAttribute("id").match(/[0-9]/gi))
+}
+function Editar (param){
 
-  quizManager.quizes[id].responder(); 
-  console.log(param.value);
-  if(quizManager.quizes[id].correcta == param.value){
+}
+function eliminar(param){
+  let id = generarid(param);
+  quizManager.deleteQuiz(param,id);
+}
+function verificar(param){
+  let id = generarid(param);
+  let quiz = quizManager.quizes.find(e => e.id == id);
+  quiz.responder(); 
+  if(quiz.correcta == param.value){
     alert("respuesta correcta");
   }
   else{
     alert("respuesta incorrecta");
   } 
   [...param.parentNode.parentNode].map(e => {
-      e.disabled = "true";
+      if(e.type != "button"){
+        e.disabled = "true";
+      }
+      
     })
   console.log(quizManager);
 }
